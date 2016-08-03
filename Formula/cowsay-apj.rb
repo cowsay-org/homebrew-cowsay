c2e769c3c8aa596a5ba27545cd67d360f0303cb7
Andrew Janke <andrew@apjanke.net>
2016-08-03 16:37:27 -0400

    cowsay-apj 3.04.01 (new formula)
    This is the main cowsay formula. It's temporarily called "cowsay-apj"
    to distinguish it from the cowsay formula in homebrew/core, but if my
    fork is adopted, this will become plain "cowsay".

---
+++ Formula/cowsay-apj.rb
@@ -0,0 +1,19 @@
+class CowsayApj < Formula
+  desc "Configurable talking characters in ASCII art"
+  homepage "https://github.com/apjanke/cowsay"
+  url "https://github.com/apjanke/cowsay/archive/v3.04.01.tar.gz"
+  sha256 "321dbe731a89cc9df441d2f0de22a506615d3ed7e2ed64b0b72acebac91de8a4"
+
+  conflicts_with "cowsay", :because => "cowsay-apj is a fork of cowsay"
+
+  def install
+    system "/bin/sh", "install.sh", prefix
+    mv prefix/"man", share
+  end
+
+  test do
+    output = shell_output("#{bin}/cowsay moo")
+    assert_match "moo", output  # bubble
+    assert_match "^__^", output # cow
+  end
+end
