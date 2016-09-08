fd322bd479e9a45d0c76ce0e320b8846c80fa767
Andrew Janke <andrew@apjanke.net>
2016-09-08 02:06:22 -0400

    update links to new website and cowsay-org GitHub org

--- Formula/cowsay-apj.rb
+++ Formula/cowsay-apj.rb
@@ -1,7 +1,7 @@
 class CowsayApj < Formula
   desc "Configurable talking characters in ASCII art"
-  homepage "https://github.com/apjanke/cowsay"
-  url "https://github.com/apjanke/cowsay/archive/v3.04.01.tar.gz"
+  homepage "https://cowsay.diamonds"
+  url "https://github.com/cowsay-org/cowsay/archive/v3.04.01.tar.gz"
   sha256 "321dbe731a89cc9df441d2f0de22a506615d3ed7e2ed64b0b72acebac91de8a4"
 
   conflicts_with "cowsay", :because => "cowsay-apj is a fork of cowsay"
