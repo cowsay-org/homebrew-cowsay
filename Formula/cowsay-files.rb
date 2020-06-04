c3e946c6640e5e1977f32dcb0c6d0bed64ee8214
Andrew Janke <andrew@apjanke.net>
2020-06-04 10:44:47 -0400

    Add cowsay-files

---
+++ Formula/cowsay-files.rb
@@ -0,0 +1,14 @@
+class CowsayFiles < Formula
+  desc "A collection of additional/alternative cowsay files"
+  homepage "https://github.com/paulkaefer/cowsay-files"
+  sha256 "af13a93f1559ea435403cf11eaa6c46be5bf03ac165a4acb471c66a4976167e0"
+  head "https://github.com/paulkaefer/cowsay-files.git"
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    true
+  end
+end
