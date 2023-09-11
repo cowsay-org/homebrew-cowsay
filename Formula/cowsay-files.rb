529b52315c18bc3642cdca639c90f0801757bced
andrewcrook <803618+andrewcrook@users.noreply.github.com>
2023-09-11 19:54:17 +0100

    use main branch
    use main branch not master

--- Formula/cowsay-files.rb
+++ Formula/cowsay-files.rb
@@ -2,7 +2,7 @@ class CowsayFiles < Formula
   desc "A collection of additional/alternative cowsay files"
   homepage "https://github.com/paulkaefer/cowsay-files"
   sha256 "af13a93f1559ea435403cf11eaa6c46be5bf03ac165a4acb471c66a4976167e0"
-  head "https://github.com/paulkaefer/cowsay-files.git"
+  head "https://github.com/paulkaefer/cowsay-files.git", branch: "main"
 
   def install
     system "make", "install", "prefix=#{prefix}"
