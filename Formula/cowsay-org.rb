716c2d00f601e940613290e0dc987d6583906199
Andrew Janke <andrew@apjanke.net>
2024-08-16 14:31:16 -0400

    cowsay-org 3.8.2

--- Formula/cowsay-org.rb
+++ Formula/cowsay-org.rb
@@ -1,8 +1,8 @@
 class CowsayOrg < Formula
   desc "Configurable talking characters in ASCII art, cowsay-org's fork"
   homepage "https://cowsay.diamonds"
-  url "https://github.com/cowsay-org/cowsay/archive/v3.8.1.tar.gz"
-  sha256 "77c0f2e55c783d878f7227f12c142686bcf8ba54c2d3f082b406d42f428090b3"
+  url "https://github.com/cowsay-org/cowsay/archive/v3.8.2.tar.gz"
+  sha256 "f6eeee3c2d4f61bc9b68b0717181f9b0affcfd174b06cd63a5b891b5f9e2383c"
   head "https://github.com/cowsay-org/cowsay.git"
 
   conflicts_with "cowsay", :because => "cowsay-org is a fork of cowsay"
