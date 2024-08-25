70df82d60c696141d5f02bb205828f807d1daece
Andrew Janke <andrew@apjanke.net>
2024-08-25 15:15:02 -0400

    Add license tag for cowsay-org, stating GPL-3.0 (not -or-later)
    Based on what I can see in the classic cowsay's Git history, the GPL re-licensing in 2016 was done with just GPL v3.0, and there was no "or later" statement in a license application document or header. So I'm treating it as probably GPL 3.0 only, but not confident enough in that to use the GPL-3.0-only license identifier (yet), so leaving it as the vaguer "GPL-3.0" for now.
    
    See https://github.com/cowsay-org/cowsay/issues/64 for ongoing investigation.

--- Formula/cowsay-org.rb
+++ Formula/cowsay-org.rb
@@ -4,6 +4,7 @@ class CowsayOrg < Formula
   url "https://github.com/cowsay-org/cowsay/archive/v3.8.3.tar.gz"
   sha256 "3bcb1f644a85792bc2ee8601971f16f8f1e7ca0013d6062cf35b4fd6d8fa29ea"
   head "https://github.com/cowsay-org/cowsay.git", branch: "main"
+  license "GPL-3.0"
 
   conflicts_with "cowsay", :because => "cowsay-org is a fork of cowsay"
 
